ee6fc32ed30b04246efe4d675d4245f273fd1693
Will <wb.premiertech@gmail.com>
2022-03-09 08:16:49 -0300

    Paginas Atualizadas

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,11 @@ import Checkout from './pages/checkout/Checkout'
 import Favorites from "./pages/favorites/Favorites"
 import Address from "./pages/address/Address"
 import Requests from "./pages/requests/Requests"
+import Faq from './pages/faq/Faq'
+import Policy_security from './pages/policy_security/Policy_security'
+import HowToBuy from './pages/howToBuy/HowToBuy';
+import AboutUs from './pages/aboutUs/AboutUs';
+import PlansVed from './pages/plansVed/PlansVed';
 
 export const Routes = () => {
     return (
@@ -18,6 +23,11 @@ export const Routes = () => {
             <Route path="/favorites" component={Favorites} />
             <Route path="/address" component={Address} />
             <Route path="/requests" component={Requests} />
+            <Route path="/faq" component={Faq}/>
+            <Route path="/policy_security" component={Policy_security}/>
+            <Route path="/howToBuy" component={HowToBuy}/>
+            <Route path="/aboutUS" component={AboutUs}/>
+            <Route path="/plansVed" component={PlansVed}/>
         </Switch>
     )
 }
