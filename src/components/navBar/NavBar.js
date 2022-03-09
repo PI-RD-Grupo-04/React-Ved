96a47e2205931ee0b65c77d397ece17490d00cc3
washington <washingtondejesuspereira@hotmail.com>
2022-03-09 09:25:34 -0300

    washington

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -8,6 +8,13 @@ function NavBar(props) {
         <>
             <Navbar bg="light" expand="lg">
                 <Container>
+                            <NavDropdown  title="Dropdown" id="basic-nav-dropdown ">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
                     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
