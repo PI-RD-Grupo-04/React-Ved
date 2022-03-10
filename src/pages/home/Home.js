70933c4118effc2ea7f6e120372c83b835cdca6e
Aldssantos <94330275+a21alex21@users.noreply.github.com>
2022-03-10 15:10:01 -0300

    atualizando

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,9 +7,7 @@ import carrosel1mel from '../../components/asserts/imagens/Carousel/carrosel1mel
 import carrosel2frutas from '../../components/asserts/imagens/Carousel/carrosel2frutas.png'
 import venhaconhecer from '../../components/asserts/imagens/Carousel/venhaconhecer.jpg'
 import ProductCard from '../../components/productCard/ProductCard'
-
-
-
+import  Button  from '..//..//components/button/Button'
 
 function Home() {
   return (
