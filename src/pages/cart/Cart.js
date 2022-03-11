cfe7aa93450c485fe4a44d60416f5dab86ee5cce
Will <wb.premiertech@gmail.com>
2022-03-11 16:38:20 -0300

    Corrigindo tela cesta imagem

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -15,73 +15,66 @@ function Cart(props) {
                         <div className="row mt-3 mb-3 text-center">
                             <h1>Sua cesta</h1>
                         </div>
-                            <ItemBasket valor={1} nome="larissa" />
-                            <ItemBasket valor={2} nome="will" />
-                            <div className="container mb-3 border">
-                                <div className="row mt-3 mb-1">
-                                    <div className="col-sm-12 col-lg-2 mt-2">
-                                        <h6>Simular Frete: </h6>
-                                    </div>
-                                    <div className="col-sm-12 col-lg-1"></div>
-                                    <div className="col-sm-12 col-lg-6 inputcep">
-                                        <input type="text" className="form-control" id="cep" required />
-                                        <span className="msg-nome  msg-error error-nome" id='cep-erro' >Apenas
-                                            Números</span>                       
-                                    </div>
+                        <ItemBasket valor={1} nome="larissa" />
+                        <ItemBasket valor={2} nome="will" />
+                        <div className="container mb-3 border">
+                            <div className="row mt-3 mb-1">
+                                <div className="col-sm-12 col-lg-2 mt-2">
+                                    <h5>Simular Frete: </h5>
                                 </div>
-                            
-                                {/* <!--cep--> */}
-                                <div className="row">
-                                    <div className="col-sm-12 col-lg-4">
-                                        <p>Entrega Comum: R$ 45,50</p>
-                                    </div>
-                                    <div className="col-sm-12 col-lg-1"></div>
-
-                                    <div className="col-sm-12 col-lg-3">
-                                        <h6>Quantidade Total De Itens: 5</h6>
-                                    </div>
-
-                                    <div className="col-sm-12 col-lg-1"></div>
-                                    <div className="col-sm-12 col-lg-3">
-                                        <h6>Total Produto: R$ 985,80</h6>
-                                    </div>
-
+                                <div className="col-sm-12 col-lg-5 inputcep">
+                                    <input type="text" className="form-control" id="cep" required />
                                 </div>
-
-                                    <div className="row">
-                                        <div className="col-sm-12 col-lg-4">
-                                            <p>Entrega Flex: R$ 60,50</p>
-                                        </div>
-                                        <div className="col-sm-12 col-lg-4"></div>
-                                        <div className="col-sm-12 col-lg-4"></div>
-                                        <div className="col-sm-12 col-lg-3">
-                                            <h6>Total Com Frete: R$ 1.031,30</h6>
-                                        </div>
-                                    </div>
+                                <div className="col-sm-12 col-lg-5"></div>
                             </div>
 
-                            <div className="text-end">
-                                <h4 className="text-dark mb-3">
-                                    Valor Total: R$ 1.031,30
-                                </h4>
+                            {/* <!--cep--> */}
+                            <div className="row">
+                                <div className="col-sm-12 col-lg-4">
+                                    <h5>Entrega Comum: R$ 45,50</h5>
+                                </div>                               
+                                <div className="col-sm-12 col-lg-4">
+                                    <h5>Quantidade Total De Itens: 5</h5>
+                                </div>
 
-                                <div className="row">
-                                    <div className="col-sm-12 col-lg-6"></div>
-                                    <div className="col-sm-12 col-lg-6">
-                                        <Button card plans delete  label='Continuar Comprando'/>
-                                    </div>
-                                    
+                                <div className="col-sm-12 col-lg-1"></div>
+                                <div className="col-sm-12 col-lg-3">
+                                    <h5>Total Produto: R$ 985,80</h5>
                                 </div>
-                                    
-                                <div className="row">
-                                    <div className="col-sm-12 col-lg-6"></div>
-                                    <div className="col-sm-12 col-lg-6">
-                                        <Button card plans success  label='Finalizar Compra'/>
-                                    </div>
+
+                            </div>
+
+                            <div className="row">
+                                <div className="col-sm-12 col-lg-4">
+                                    <h5>Entrega Flex: R$ 60,50</h5>
                                 </div>
-                               
-                                
-                            </div>                        
+                                <div className="col-sm-12 col-lg-4"></div>
+                                <div className="col-sm-12 col-lg-1"></div>
+                                <div className="col-sm-12 col-lg-3">
+                                    <h5>Total Com Frete: R$ 1.031,30</h5>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="text-end">
+                            <h5 className="text-dark mb-3">
+                                Valor Total: R$ 1.031,30
+                            </h5>                          
+                            <div className="row">
+                                <div className="col-sm-12 col-lg-6"></div>
+                                <div className="col-sm-12 col-lg-6">
+                                    <Button card plans delete label='Continuar Comprando' />
+                                </div>
+
+                            </div>
+
+                            <div className="row">
+                                <div className="col-sm-12 col-lg-6"></div>
+                                <div className="col-sm-12 col-lg-6">
+                                    <Button card plans success label='Finalizar Compra' />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </main>
             </div>
