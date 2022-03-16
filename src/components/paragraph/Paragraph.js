50142467805fe7c7f6d7bba8f51bba4d4c468e0b
Larissa <lmpaixao@rd.com.br>
2022-03-16 20:42:13 -0300

    adicionando componente

---
+++ src/components/paragraph/Paragraph.js
@@ -0,0 +1,23 @@
+import "./Paragraph.css"
+
+
+function Paragraph (props) {
+   
+    
+    return (  
+        <div>
+            
+
+
+
+        </div>
+
+
+      )
+
+
+
+
+
+}
+export default Paragraph
