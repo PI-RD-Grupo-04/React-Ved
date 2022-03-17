075f518942670e313473ffc88d1fd7f157e1b2ac
Larissa <lmpaixao@rd.com.br>
2022-03-16 22:09:27 -0300

    adicionando paragraph

--- src/pages/faq/Faq.js
+++ src/pages/faq/Faq.js
@@ -1,6 +1,7 @@
 import './Faq.css'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
+import Paragraph from '../../components/paragraph/Paragraph'
 
 function Faq() {
     return (
@@ -11,51 +12,40 @@ function Faq() {
                 <br /><br /><br />
                 <h2 className='faq2'>Entrega em caixa postal:</h2>
                 <br />
-                <div class="row">
-                    <p>
-                        Não efetuamos entregas em caixa postal. Entrega sujeita à devolução.
-                    </p>
-                </div>
+               
+                <Paragraph>
+                    Não efetuamos entregas em caixa postal. Entrega sujeita à devolução.
+                </Paragraph>
                 <br /><br />
                 <h2 className='faq2'>Fique Atento:</h2>
                 <br />
-                <div class="row">
-                    <p>
-                        O prazo de entrega passa a contar após a aprovação do pagamento. Assim que seu pagamento for aprovado, você será notificado por e-mail.
-                    </p>
-                </div>
-
-                <div class="row">
-                    <p>
-                        Todos os pedidos estão sujeitos à análise, aprovação de dados cadastrais e confirmação de estoque.
-                    </p>
-                </div>
+                
+                <Paragraph>
+                O prazo de entrega passa a contar após a aprovação do pagamento. Assim que seu pagamento for aprovado, você será notificado por e-mail.
+                </Paragraph>
+                
+                <Paragraph>
+                Todos os pedidos estão sujeitos à análise, aprovação de dados cadastrais e confirmação de estoque.
+                </Paragraph>
                 <br /><br />
                 <h2>Política da Empresa:</h2>
                 <br />
 
-                <div class="row">
-                    <p>
-                        Para saber o valor e prazo do frete, clique no produto, em seguida você será redirecionado para seu carrinho, onde poderá finalizar seu pedido, lá todos os prazos serão calculados automaticamente, e caso decida mudar algum item poderá fazer todas alterações que desejar tranquilamente e assim que estiver certo poderá finalizar seu pedido.
-                    </p>
-                </div>
+                <Paragraph>
+                Para saber o valor e prazo do frete, clique no produto, em seguida você será redirecionado para seu carrinho, onde poderá finalizar seu pedido, lá todos os prazos serão calculados automaticamente, e caso decida mudar algum item poderá fazer todas alterações que desejar tranquilamente e assim que estiver certo poderá finalizar seu pedido.
+                </Paragraph>
                 <br /><br />
                 <h2 className='faq2'>Política de Preço:</h2>
                 <br />
 
-                <div class="row">
-                    <p>
-                        ATENÇÃO!! Os preços dentro de nossa loja virtual podem ser diferentes de loja física devido a diferenciação de custos operacionais.
-                    </p>
-                </div>
+                <Paragraph>
+                ATENÇÃO!! Os preços dentro de nossa loja virtual podem ser diferentes de loja física devido a diferenciação de custos operacionais.
+                </Paragraph>
                 <br /><br /><br />
-                <h2>Entrega em caixa postal:</h2>
-                <br />
-                <div class="row">
-                    <p>
-                        Não efetuamos entregas em caixa postal. Sujeito à devolução.
-                    </p>
-                </div>
+               
+                <Paragraph>
+                Não efetuamos entregas em caixa postal. Sujeito à devolução.
+                </Paragraph>
                 <br /><br />
 
             </div>
