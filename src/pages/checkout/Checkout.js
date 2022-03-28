c8e66a9a27b6490af9abcfcfdbb91e07fb8b6ab8
jefferson <guinho.psp1@gmail.com>
2022-03-28 09:04:11 -0300

    organizando chekout

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -19,13 +19,10 @@ class Checkout extends Component {
         paymentForm: {
             card: false,
             pix: false,
-            boleto: false
+            boleto: false,
+            cpfBoleto: false
+
         }
-    }
-
-
-    methodpayment = () => {
-
     }
 
 
@@ -48,16 +45,51 @@ class Checkout extends Component {
         return (
             <div className="row gy-3 ">
                 <div className="col-12 d-flex flex-column align-items-center justify-content-center">
-                    <h2 className="text-center mb-2">Boleto gerado!</h2>
+                    <h2 className="text-center mb-2">Boleto gerado </h2>
                     <textarea className="boleto border" disabled >34191.79001 01043.510047 91020.150008 7 89250026000</textarea>
                     <h4>ou </h4>
                     <div className="container d-grid gy-2 mb-3">
-                        <Button label="Acesse aqui" sucess card />
+                       <a className="btn btn-success" href="http://www.sicadi.com.br/mhouse/boleto/boleto3.php?numero_banco=341-7&local_pagamento=PAG%C1VEL+EM+QUALQUER+BANCO+AT%C9+O+VENCIMENTO&cedente=VED+-+ALIMENTOS+ORG%C2NICOS+&data_documento=25%2F03%2F2022&numero_documento=DF+00113&especie=&aceite=N&data_processamento=25%2F03%2F2022&uso_banco=&carteira=179&especie_moeda=Real&quantidade=2&valor=1900&vencimento=25%2F03%2F2022&agencia=0049&codigo_cedente=10201-5&meunumero=00010435&valor_documento=260%2C00&instrucoes=Taxa+de+visita+de+suporte%0D%0AAp%F3s+o+vencimento+R%24+0%2C80+ao+dia&mensagem1=&mensagem2=&mensagem3=ATEN%C7%C3O%3A+N%C3O+RECEBER+AP%D3S+15+DIAS+DO+VENCIMENTO&sacado=&Submit=Enviar">Acesse aqui</a>
                     </div>
+                  
                 </div>
             </div>
         )
     }
+
+
+    ativaBoleto = () => this.setState({
+        paymentForm: {
+            card: false,
+            pix: false,
+            boleto: true,
+            cpfBoleto: false
+        }
+    })
+
+
+
+    preBoleto = () => {
+
+        return (
+            <div class="row gy-3 ">
+
+                <div class="col-12 d-flex flex-column align-items-center justify-content-center">
+                    <h2 class="text-center mb-2">Informações para o Boleto</h2>
+                    <label for="nomeboleto">Nome:</label>
+                    <input type="text" id="nomeboleto" class="form-control" />
+                    <label for="nomecpf">CPF:</label>
+                    <input type="text" id="cpfboleto" class="form-control" />
+
+                    <div class="container mt-4 d-grid gy-2 mb-3"  onClick={this.ativaBoleto}>
+                        <Button success label="gera boleto" />
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+
 
     creditcard = () => {
         return (
@@ -233,23 +265,27 @@ class Checkout extends Component {
                                                 paymentForm: {
                                                     card: false,
                                                     pix: false,
-                                                    boleto: true
+                                                    boleto: false,
+                                                    cpfBoleto: true
+
                                                 }
-                                            })} label="Boleto" id={1} name="1" />
+                                            })} label="Boleto" id='boleto' name="1" />
                                             <RadioBox onClick={() => this.setState({
                                                 paymentForm: {
                                                     card: true,
                                                     pix: false,
-                                                    boleto: false
+                                                    boleto: false,
+                                                    cpfBoleto: false
                                                 }
-                                            })} label="cartão de credito/debito" id={2} name="1" />
+                                            })} label="cartão de credito/debito" id="card" name="1" />
                                             <RadioBox onClick={() => this.setState({
                                                 paymentForm: {
                                                     card: false,
                                                     pix: true,
-                                                    boleto: false
+                                                    boleto: false,
+                                                    cpfBoleto: false
                                                 }
-                                            })} label="Pix" id={1} name="1" />
+                                            })} label="Pix" id='pix' name="1" />
                                         </div>
 
 
@@ -257,6 +293,7 @@ class Checkout extends Component {
                                         {this.state.paymentForm.card ? this.creditcard() : ""}
                                         {this.state.paymentForm.pix ? this.pix() : ""}
                                         {this.state.paymentForm.boleto ? this.boleto() : ""}
+                                        {this.state.paymentForm.cpfBoleto ? this.preBoleto() : ""}
                                         <hr className="my-4 mb-3" />
                                     </div>
                                     <div className="d-grid gy-2">
